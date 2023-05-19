68c91a60030de7c853cbba5470a5fe42857cbec2
Mark Albrand <mark.albrand5@gmail.com>
2023-05-19 00:10:18 -0600

    correction

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -20,8 +20,6 @@ function Body({ buttons, handleClick }) {
                             buttonClass += ` ${styles.Zero}`
                         }
 
-                        buttonClass += "button-for-test"
-
                         return (
                             <button
                                 key={button}
