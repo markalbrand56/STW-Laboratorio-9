34b9631a8f1da129043ac2e6bdd6fd8b30071224
Mark Albrand <mark.albrand5@gmail.com>
2023-05-18 18:33:30 -0600

    Start

---
+++ src/Components/Calculator.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import styles from "./Calculator.module.css"
+
+function Calculator() {
+    return (
+        <div className={styles.calculator}>
+            <p>Calculadora</p>
+        </div>
+    )
+}
+
+export default Calculator
